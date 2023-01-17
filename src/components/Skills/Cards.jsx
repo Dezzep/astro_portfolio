a36fdc9cf5928c97d177c28120656946bcbaa0a3
Jacob Handelman <jacobhandelman@gmail.com>
2023-01-17 13:08:28 -0500

    removed the github component to see if that was causing the issue:

--- src/components/Skills/Cards.jsx
+++ src/components/Skills/Cards.jsx
@@ -1,9 +1,9 @@
-const Card = ({ item, title }) => {
+const Card = (props) => {
   return (
     <div className=" sm:px-6 pb-6 ">
       <div className="py-6 mx-1 min-[500px]:mx-2 sm:px-6 w-32 min-[500px]:w-40 sm:w-48 flex flex-col justify-center border rounded-lg hover:shadow-lg border-purpMain-50/60 hover:shadow-purpMain-50/80">
-        <div className="text-7xl mb-2 mx-auto">{item}</div>
-        <p className=" text-center">{title}</p>
+        <div className="text-7xl mb-2 mx-auto">{props.item}</div>
+        <p className=" text-center">{props.title}</p>
       </div>
     </div>
   );
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,11 +1,9 @@
 import Technology from './Technology.jsx';
-import Github from './Github.jsx';
 
 const Skills = () => {
   return (
     <div className="bg-black min-h-screen">
       <Technology />
-      <Github />
     </div>
   );
 };
