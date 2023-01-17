f4d2339efe2a81072afca780ad2c49764a8a2737
Jacob Handelman <jacobhandelman@gmail.com>
2023-01-17 12:47:17 -0500

    changed an astro file to a jsx file to see if it will fix an issue on netlify

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,13 @@
+import Technology from './Technology.jsx';
+import Github from './Github.jsx';
+
+const Skills = () => {
+  return (
+    <div className="bg-black min-h-screen">
+      <Technology />
+      <Github />
+    </div>
+  );
+};
+
+export default Skills;
